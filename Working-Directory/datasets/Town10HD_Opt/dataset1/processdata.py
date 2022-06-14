0042bb30cfeb732f47a79a0b2eb1a1fd0e9fc6f9
airwick989 <ridwanhossain989@gmail.com>
2022-06-14 15:27:30 -0400

    began processing data collected in dataset1

---
+++ Working-Directory/datasets/Town10HD_Opt/dataset1/processdata.py
@@ -0,0 +1,7 @@
+from turtle import title
+import pandas as pd
+import plotly.express as px
+
+vehicle_telemetry = pd.read_csv('vehicle_telemetry.csv')
+speed_plot = px.line(vehicle_telemetry, x = 'Rec_time', y = 'Speed', title = 'Vehicle Speed')
+speed_plot.write_image("testimage.png")
