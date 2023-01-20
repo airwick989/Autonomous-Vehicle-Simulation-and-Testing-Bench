31401eba96061182f2c88f00d1bbe43e389e2218
airwick989 <73313597+airwick989@users.noreply.github.com>
2023-01-20 17:55:46 -0500

    set up training parameters for iforest model

---
+++ Working-Directory-v2/NRC/Code/anomaly_detection.py
@@ -0,0 +1,21 @@
+from pycaret.anomaly import *
+import pandas as pd
+
+UNSEEN_DATA_SIZE = 10000
+data = []
+with open('../Files/candump-2022-12-14_100719.log', 'r') as f:
+    for line in f:
+        line = line.split(" ")
+        line = line[2]
+        line = line.strip("\n")
+        data.append(line)
+
+df_train = data[0:-UNSEEN_DATA_SIZE]
+df_train = pd.DataFrame(df_train, columns=['data'])
+df_unseen = data[-UNSEEN_DATA_SIZE:]
+df_unseen = pd.DataFrame(df_unseen, columns=['data'])
+
+anom = setup(data= df_train, silent=True)
+anom_model = create_model(model = 'iforest')
+results = assign_model(anom_model)
+save_model(model = anom_model, model_name='anomaly_detection_model')
