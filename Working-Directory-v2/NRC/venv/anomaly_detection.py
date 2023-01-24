40e7d8c86f66244e5e91c3922ad651ee2f0846c8
airwick989 <73313597+airwick989@users.noreply.github.com>
2023-01-24 16:58:19 -0500

    Output processed data resulting from predictions made by the anomaly detection model for time intervals

--- Working-Directory-v2/NRC/venv/anomaly_detection.py
+++ Working-Directory-v2/NRC/venv/anomaly_detection.py
@@ -2,8 +2,10 @@ from pycaret.anomaly import load_model, predict_model
 import pandas as pd
 
 UNSEEN_DATA_SIZE = 10000
-results_path = '../Files/Results/Messages/'
+messages_results_path = '../Files/Results/Messages/'
+intervals_results_path = '../Files/Results/Intervals/'
 
+#Parse the log file
 data = []
 timestamps = []
 with open('../Files/candump-2022-12-14_100719.log', 'r') as f:
@@ -21,7 +23,7 @@ df_unseen = data[-UNSEEN_DATA_SIZE:]
 df_unseen = pd.DataFrame(df_unseen, columns=['data'])
 timestamps = timestamps[-UNSEEN_DATA_SIZE:]
 
-# print(df_unseen)
+#MESSAGES#####################################################################################################################################################
 
 #Use saved model to perform predictions
 anom_model = load_model('anomaly_detection_model')
@@ -49,7 +51,7 @@ str_anom_uniq = "\n"
 for anom in anom_uniq:
     str_anom_uniq = str_anom_uniq + str(anom) + "\n"
 
-with open(f'{results_path}message_anomaly_statistics.txt', 'w+') as f:
+with open(f'{messages_results_path}message_anomaly_statistics.txt', 'w+') as f:
     f.write(
         f'''Anomalous Percentage: {anom_percentage}% 
         \n{anom_count}/{UNSEEN_DATA_SIZE} messages were predicted to be anomalous.
@@ -61,5 +63,76 @@ with open(f'{results_path}message_anomaly_statistics.txt', 'w+') as f:
 ####
 
 #Save predictions to csv
-predicitons.to_csv(f'{results_path}message_anomaly_predictions.csv', encoding='utf-8')
-anom_samples.to_csv(f'{results_path}message_anomalies.csv', encoding='utf-8')
+predicitons.to_csv(f'{messages_results_path}message_anomaly_predictions.csv', encoding='utf-8')
+anom_samples.to_csv(f'{messages_results_path}message_anomalies.csv', encoding='utf-8')
+
+message_predictions = predicitons
+message_anom_samples = anom_samples
+
+print("CAN bus message data processed")
+
+
+#INTERVALS###################################################################################################################################################################
+
+
+
+#Prepare the data
+df_unseen = timestamps
+df_unseen = list(map(float, df_unseen))
+for i in range(len(df_unseen) - 1, 0, -1):
+    df_unseen[i] = df_unseen[i] - df_unseen[i-1]
+del df_unseen[0]
+df_unseen = pd.DataFrame(df_unseen, columns=['intervals'])
+messages = data[-UNSEEN_DATA_SIZE:]
+del messages[0]
+
+#Use saved model to perform predictions
+anom_model = load_model('interval_anomaly_detection_model')
+predicitons = predict_model(model=anom_model, data=df_unseen)
+del timestamps[0]
+predicitons['timestamps'] = timestamps
+
+####
+
+#Numeric Results
+results = predicitons['Anomaly'].value_counts()
+anom_count = results[1]
+anom_percentage = round((anom_count/UNSEEN_DATA_SIZE)*100, 2)
+
+anom_samples = predicitons[predicitons['Anomaly'] == 1]
+anom_messages = []
+for index in anom_samples.index.values.tolist():
+    anom_messages.append(messages[index])
+anom_samples['Data'] = anom_messages
+anom_samples = anom_samples.drop('Anomaly', axis=1)
+
+anom_mean = anom_samples['intervals'].mean()
+anom_min_index = anom_samples[anom_samples['intervals'] == anom_samples['intervals'].min()].index[0]
+anom_max_index = anom_samples[anom_samples['intervals'] == anom_samples['intervals'].max()].index[0]
+
+with open(f'{intervals_results_path}intervals_anomaly_statistics.txt', 'w+') as f:
+    f.write(
+        f'''Anomalous Percentage: {anom_percentage}% 
+        \n{anom_count}/{UNSEEN_DATA_SIZE} messages were predicted to be anomalous.
+        \nThe mean value of an anomalous time interval: {anom_mean}
+        \nThe smallest anomalous time interval is: {anom_samples['intervals'][anom_min_index]} (Message: {anom_samples['Data'][anom_min_index]}, Timestamp: {anom_samples['timestamps'][anom_min_index]})
+        \nThe largest anomalous time interval is: {anom_samples['intervals'][anom_max_index]} (Message: {anom_samples['Data'][anom_max_index]}, Timestamp: {anom_samples['timestamps'][anom_max_index]})
+    ''')
+
+#Save predictions to csv
+predicitons.to_csv(f'{intervals_results_path}interval_anomaly_predictions.csv', encoding='utf-8')
+anom_samples.to_csv(f'{intervals_results_path}interval_anomalies.csv', encoding='utf-8')
+
+interval_predictions = predicitons
+interval_anom_samples = anom_samples
+
+print("CAN bus message intervals processed\n\n\n\n\n\n")
+
+
+#BOTH########################################################################################################################################################################
+
+
+print(message_predictions.head())
+print(message_anom_samples.head())
+print(interval_predictions.head())
+print(interval_anom_samples.head())
--- Working-Directory-v2/NRC/venv/interval_anomaly_training.py
+++ Working-Directory-v2/NRC/venv/interval_anomaly_training.py
@@ -2,7 +2,7 @@ from pycaret.anomaly import setup, create_model, assign_model, save_model
 import pandas as pd
 
 UNSEEN_DATA_SIZE = 10000
-results_path = '../Files/Results/Timestamps/'
+results_path = '../Files/Results/Intervals/'
 
 timestamps = []
 with open('../Files/candump-2022-12-14_100719.log', 'r') as f:
@@ -31,8 +31,8 @@ df_train = pd.DataFrame(df_train, columns=['intervals'])
 
 ####
 
-#Training and saving the model
-anom = setup(data= df_train, silent=True)
-anom_model = create_model(model = 'iforest')
-results = assign_model(anom_model)
-save_model(model = anom_model, model_name='interval_anomaly_detection_model')
+# #Training and saving the model
+# anom = setup(data= df_train, silent=True)
+# anom_model = create_model(model = 'iforest')
+# results = assign_model(anom_model)
+# save_model(model = anom_model, model_name='interval_anomaly_detection_model')
